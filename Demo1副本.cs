8a17a132477df890d0d39b66811d3850808f7140
Sarah7bei <458248232@qq.com>
2016-09-26 11:53:19 +0800

    k11
    123

---
+++ Demo1副本.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo1 : MonoBehaviour {
+
+
+	void Start () {
+//		Debug.Log ("log");
+//		Debug.LogError ("Error");
+//		Debug.LogWarning ("Warning");
+//		Debug.LogFormat ("{0},{1}",1,2);
+//		print ("kobe is mvp!");
+		//System.Console.WriteLine ("kobe");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
